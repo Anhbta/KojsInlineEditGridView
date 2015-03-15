87493fbb6812e06936c0d2856d3f59e4c8f85206
Anh Bui <anh.buitrong@gmail.com>
2015-03-15 14:09:08 +0800

    Add sorting

--- KojsInlineEditGridview/KojsInlineEditGridview/Models/BaseCriteria.cs
+++ KojsInlineEditGridview/KojsInlineEditGridview/Models/BaseCriteria.cs
@@ -10,6 +10,6 @@ namespace KojsInlineEditGridview.Models
         public int PageIndex { get; set; }
         public int PageSize { get; set; }
         public string SortExpression { get; set; }
-        public string SortDirection { get; set; }
+        public string SortOrder { get; set; }
     }
 }
